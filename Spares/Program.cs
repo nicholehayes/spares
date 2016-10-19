663c8bd36d2006348e1e1b36bf909efd3be492ce
nicholehayes <nhayes5@lsu.edu>
2016-10-19 01:48:30 -0500

    Add files via upload
    The program's now working properly. Strikes and spares are accounted for, with the appropriate amount of extra throws. I split the main method up into much more readable, specific functions.

--- Spares/Program.cs
+++ Spares/Program.cs
@@ -6,207 +6,157 @@ namespace Spares {
 
 		public static void Main (string[] args) {
 		
-			//TODO: add support for multiplayer.
-
-			string[] symbolScoreArray = new string[10];
-			string[] roundScoreArray = new string[10];
-			string[] totalScoreArray = new string[10];
-			bool isStrikeOrSpare = false;
-			int numPinsDown;
-			int numPinsLeft;
-			int currentThrow;
-			int currentRound;
-			int roundScore;
-			int totalScore = 0;
-
+			//TODO: Add a new row in the scoreboard that shows symbols for strikes/spares/open frames.
+			//TODO: Implement special tenth frame properly. (It works right now; could be made better)
+			//TODO: Implement a way for users to specify how many frames they want.
+		
 			Console.WriteLine ("Welcome to Spares!\n");
+			playGame();
+		}
+			
+		//
+		public static void playGame(){
+		
+			int currentFrame;
+			int frameScore;
+			int totalScore = 0;
+			//string[] symbolScoreArray = new string[10]; (Move to separate function)
+			string[] frameScoreArray = new string[10];
+			string[] totalScoreArray = new string[10];
 
-			for(currentRound = 1; currentRound <= 3; currentRound++) {
+			//Plays a game of 10 frames, resets the frame score for each 
+			//and draws the scoreboard after each frame
+			for(currentFrame = 1; currentFrame <= 10; currentFrame++) {
+				frameScore = 0;
 
-				roundScore = 0;
-				numPinsLeft = 10;
-				isStrikeOrSpare = false;
+				frameScore = frame(currentFrame.ToString());
 
-				/*
-				for (currentThrow = 1; currentThrow <= 2; currentThrow++){
-					Console.Write ("ROUND " + currentRound + " THROW " + currentThrow + ": Enter the number of pins knocked down: ");
-					numPinsDown = Convert.ToInt32(Console.ReadLine ());
+				totalScore += frameScore;
 
+				//createSymbolArray ();
+				frameScoreArray [currentFrame - 1] = frameScore.ToString();
+				totalScoreArray [currentFrame - 1] = totalScore.ToString();
 
-					//Spares can be dealt with better
-					//if (currentThrow == 2 && numPinsDown == numPinsStanding){
-					//	spareTime();
-					//	break;
-					//}
-
-					if(currentThrow == 1 && numPinsDown == numPinsLeft){
-						roundScore += strikeTime ();
-						break; 				//Maybe this is wrong! I want to get out of the inner 'for', not the outer. (return?)
-					}
-						
-					if (numPinsDown <= numPinsLeft){
-						numPinsLeft -= numPinsDown;
-						roundScore += numPinsDown;
-						Console.WriteLine (numPinsDown + " pin(s) hit! " + numPinsLeft + " pin(s) left.");
-
-					}else{
-						Console.WriteLine ("Please enter a number of pins that is less than or equal to the current standing number of pins.");
-						//throw error, return to console.read line (?)
-					}
-				}
-				*/
-
-				roundScore = twoThrows(currentRound, isStrikeOrSpare);
-
-				totalScore += roundScore;
-
-				roundScoreArray [currentRound - 1] = roundScore.ToString();
-				totalScoreArray [currentRound - 1] = totalScore.ToString();
-
-				drawScoreboard (roundScoreArray, totalScoreArray);
-
-				//Console.WriteLine ("Round Score: " + roundScore);
-				//Console.WriteLine ("Total Score: " + totalScore);
+				drawScoreboard (frameScoreArray, totalScoreArray);
 			}
-			Console.WriteLine ("");
+
+			Console.WriteLine ("Game Over! Total Score: " + totalScore);
+		}
+
+
+		//The start of a frame without regarding strikes/spares
+		public static int frame(string currentFrame){
+		
+			int currentThrow;
+			int numPinsDown;
+			int frameScore = 0;
+			int numPinsLeft = 10;
+
+			for (currentThrow = 1; currentThrow <= 2; currentThrow++) {
+			
+				numPinsDown = throwBall (currentFrame, currentThrow, numPinsLeft);
+
+				//If strike
+				if (currentThrow == 1 && numPinsDown == numPinsLeft) {
+					frameScore += numPinsDown;
+					frameScore += strike ();
+					break; //This should work
+				}
+
+				//If spare
+				else if (currentThrow == 2 && numPinsDown == numPinsLeft) {
+					frameScore += numPinsDown;
+					frameScore += spare ();
+					break; //This should work
+				}
+
+				//Else it's an open frame
+				else if (numPinsDown <= numPinsLeft) {
+					numPinsLeft -= numPinsDown;
+					frameScore += numPinsDown;
+					Console.WriteLine (numPinsDown + " pin(s) hit! " + numPinsLeft + " pin(s) left."); //Could this be a seperate funciton?
+				}
+
+			}
+
+			return frameScore;
+		}
+
+
+		public static int throwBall(string currentFrame, int currentThrow, int numPinsLeft){
+			int numPinsDown;
+
+			while (true) {
+				Console.Write ("FRAME " + currentFrame + " THROW " + currentThrow + ": Enter the number of pins knocked down: ");
+				numPinsDown = Convert.ToInt32 (Console.ReadLine ());
+
+				if (0 <= numPinsDown && numPinsDown <= numPinsLeft) {
+					return numPinsDown;
+				} else {
+					Console.WriteLine ("Please enter a number of pins that is less than or equal to the current standing number of pins.");
+				}
+			}
+		}
+
+
+		public static int strike(){
+			string currentFrame = "STRIKE";
+			int currentThrow;
+			int numPinsLeft = 10;
+			int numPinsDown;
+			int strikeScore = 0;
+
+			Console.WriteLine ("STRIKE!!");
+
+			for (currentThrow = 1; currentThrow <= 2; currentThrow++) {
+
+				numPinsDown = throwBall (currentFrame, currentThrow, numPinsLeft);
+
+				numPinsLeft -= numPinsDown;
+				strikeScore += numPinsDown;
+				Console.WriteLine (numPinsDown + " pin(s) hit! " + numPinsLeft + " pin(s) left."); //Could this be a seperate funciton?
+
+				//Reset pins if they were all knocked down
+				if (numPinsLeft == 0){
+					numPinsLeft = 10;
+				}
+			}
+			Console.WriteLine ("Strike Round Score: " + strikeScore);
+			return strikeScore;
+
 		}
 			
 
+		public static int spare(){
+			string currentFrame = "SPARE";
+			int currentThrow = 1;
+			int numPinsLeft = 10;
+			int spareScore = 0;
+
+			Console.WriteLine ("SPARE!");
+			spareScore += throwBall (currentFrame, currentThrow, numPinsLeft);
+
+			return spareScore;
+		}
+			
 
 		//TODO: Add symbol row and final score (maybe). This works fine right now.
 		public static void drawScoreboard(string[] roundScoreArray, string[] totalScoreArray){
-			Console.WriteLine (" #####################################################################################");
+
+			Console.WriteLine (" ###########################################################################");
+			Console.WriteLine (" #    FRAME    #   1 #   2 #   3 #   4 #   5 #   6 #   7 #   8 #   9 #  10 #");
+			Console.WriteLine (" ###########################################################################");
 			Console.Write (" # ROUND SCORE");
 			for (int i = 0; i < 10; i++){
-				Console.Write(String.Format(" # {0,4}", roundScoreArray[i]));
+				Console.Write(String.Format(" # {0,3}", roundScoreArray[i]));
 			}
-			Console.Write (" # \n ##################################################################################### \n");
+			Console.Write (" # \n ########################################################################### \n");
 			Console.Write (" # TOTAL SCORE");
 			for (int i = 0; i < 10; i++){
-				Console.Write(String.Format(" # {0,4}", totalScoreArray[i]));
+				Console.Write(String.Format(" # {0,3}", totalScoreArray[i]));
 			}
-			Console.Write (" # \n #####################################################################################");
-			Console.WriteLine ();
+			Console.Write (" # \n ###########################################################################");
+			Console.WriteLine ("\n");
 		}
-
-
-
-
-		//public static int strikeTime(){
-			//int roundScore = 10;
-			//int numPinsLeft = 10;
-			//int numPinsDown;
-			//int strikeThrow;
-
-			//int roundScore = 0;
-
-			//Console.WriteLine ("STRIKE!!");
-			//roundScore = twoThrows();
-			//Console.WriteLine ("Strike Round Score: " + roundScore);
-			//return roundScore;
-
-
-			/*
-			for (strikeThrow = 1; strikeThrow <= 2; strikeThrow++) {
-				Console.Write ("STRIKE THROW " + strikeThrow + ": Enter the number of pins knocked down: ");
-				numPinsDown = Convert.ToInt32(Console.ReadLine ());
-
-				if (numPinsDown < numPinsLeft) {
-					numPinsLeft -= numPinsDown;
-					roundScore += numPinsDown;
-
-					Console.WriteLine (numPinsDown + " pins hit! " + numPinsLeft + " pins left.");
-
-				} else if (numPinsDown == 10) {
-					roundScore += numPinsDown;
-					numPinsLeft = 10;
-					Console.WriteLine ("STRIKE!! Strike Round Score: " + roundScore);
-				}else {
-					Console.WriteLine ("You shouldn't see me!");
-					//throw error, return to console.read line (?)
-				}
-			}
-			Console.WriteLine ("Strike Round Score: " + roundScore);
-			return roundScore;
-			*/
-		//}
-
-
-
-		//public static int spareTime(){
-		//	Console.WriteLine ("SPARE!");
-		//}
-			
-
-
-
-		public static int twoThrows(int currentRound, bool isStrikeOrSpare){
-
-			bool correctUserInput;
-			int currentThrow = 1;
-			int numPinsDown;
-			int roundScore = 0;
-			int numPinsLeft = 10;
-
-
-			for (currentThrow = 1; currentThrow <= 2; currentThrow++){
-
-				correctUserInput = false;
-
-				while (correctUserInput == false){
-
-					if (isStrikeOrSpare == false) {
-						Console.Write ("ROUND " + currentRound + " THROW " + currentThrow + ": Enter the number of pins knocked down: ");
-					} else {
-						Console.Write ("STRIKE ROUND THROW " + currentThrow + ": Enter the number of pins knocked down: ");
-					}
-
-					numPinsDown = Convert.ToInt32 (Console.ReadLine ());
-
-
-					//If strike
-					if(currentThrow == 1 && numPinsDown == numPinsLeft){
-						correctUserInput = true;
-						roundScore += numPinsDown;
-
-						if (isStrikeOrSpare == false) {
-							isStrikeOrSpare = true;
-							Console.WriteLine ("STRIKE!!");
-							roundScore += twoThrows (currentRound, isStrikeOrSpare);
-						}
-
-						//roundScore += strikeTime ();
-						//strikeTime ();
-					}
-
-					//If spare
-					else if (currentThrow == 2 && numPinsDown == numPinsLeft){
-						correctUserInput = true;
-						isStrikeOrSpare = true;
-						//spareTime();
-					}
-
-					//Else it's a normal hit
-					else if (numPinsDown <= numPinsLeft){
-						correctUserInput = true;
-						numPinsLeft -= numPinsDown;
-						roundScore += numPinsDown;
-						Console.WriteLine (numPinsDown + " pin(s) hit! " + numPinsLeft + " pin(s) left.");
-					}
-
-					//Error catcher
-					else{
-						Console.WriteLine ("Please enter a number of pins that is less than or equal to the current standing number of pins.");
-					}
-
-				}
-
-			}
-
-			return roundScore;
-
-
-		}
-
 	}
 }
